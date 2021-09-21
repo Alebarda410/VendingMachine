e79511573e6cb90fdda9d60d9689076338a249e0
Dmitry Druzhin <donat410@gmail.com>
2021-09-21 11:53:01 +0300

    Добавленный данные для заполнения БД

---
+++ dbo.Coins.data.sql
@@ -0,0 +1,6 @@
+SET IDENTITY_INSERT [dbo].[Coins] ON
+INSERT INTO [dbo].[Coins] ([Id], [Denomination], [Count], [Availability]) VALUES (3, 1, 100, 1)
+INSERT INTO [dbo].[Coins] ([Id], [Denomination], [Count], [Availability]) VALUES (4, 2, 98, 0)
+INSERT INTO [dbo].[Coins] ([Id], [Denomination], [Count], [Availability]) VALUES (5, 5, 95, 1)
+INSERT INTO [dbo].[Coins] ([Id], [Denomination], [Count], [Availability]) VALUES (6, 10, 105, 1)
+SET IDENTITY_INSERT [dbo].[Coins] OFF
---
+++ dbo.Drinks.data.sql
@@ -0,0 +1,13 @@
+SET IDENTITY_INSERT [dbo].[Drinks] ON
+INSERT INTO [dbo].[Drinks] ([Id], [Name], [Logo], [Price], [Count], [Availability]) VALUES (28, N'Закамский квас', N'/upload/28_logo.png', 50, 10, 1)
+INSERT INTO [dbo].[Drinks] ([Id], [Name], [Logo], [Price], [Count], [Availability]) VALUES (29, N'Кока кола', N'/upload/29_logo.png', 10, 42, 1)
+INSERT INTO [dbo].[Drinks] ([Id], [Name], [Logo], [Price], [Count], [Availability]) VALUES (30, N'Пепси', N'/upload/30_logo.png', 10, 5, 0)
+INSERT INTO [dbo].[Drinks] ([Id], [Name], [Logo], [Price], [Count], [Availability]) VALUES (31, N'Спрайт', N'/upload/31_logo.png', 8, 0, 1)
+INSERT INTO [dbo].[Drinks] ([Id], [Name], [Logo], [Price], [Count], [Availability]) VALUES (32, N'Байкал', N'/upload/32_logo.png', 12, 24, 1)
+INSERT INTO [dbo].[Drinks] ([Id], [Name], [Logo], [Price], [Count], [Availability]) VALUES (33, N'Фанта', N'/upload/33_logo.png', 8, 11, 1)
+INSERT INTO [dbo].[Drinks] ([Id], [Name], [Logo], [Price], [Count], [Availability]) VALUES (34, N'Вода без газа', N'/upload/34_logo.png', 2, 322, 1)
+INSERT INTO [dbo].[Drinks] ([Id], [Name], [Logo], [Price], [Count], [Availability]) VALUES (35, N'Вода с газом', N'/upload/35_logo.png', 4, 99, 1)
+INSERT INTO [dbo].[Drinks] ([Id], [Name], [Logo], [Price], [Count], [Availability]) VALUES (36, N'Швепс', N'/upload/36_logo.png', 20, 4, 1)
+INSERT INTO [dbo].[Drinks] ([Id], [Name], [Logo], [Price], [Count], [Availability]) VALUES (37, N'Тархун', N'/upload/37_logo.png', 12, 12, 1)
+INSERT INTO [dbo].[Drinks] ([Id], [Name], [Logo], [Price], [Count], [Availability]) VALUES (38, N'Лимонад', N'/upload/38_logo.png', 5, 40, 1)
+SET IDENTITY_INSERT [dbo].[Drinks] OFF
